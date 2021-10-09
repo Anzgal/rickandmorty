f80dc3162e8c4dbdb1dcd0cdea13360159927883
Anzgal <agalvan70@hotmail.com>
2021-10-09 11:45:56 -0500

    Minor changes

--- src/store.js
+++ src/store.js
@@ -7,44 +7,6 @@ const initialState = {
   currentFetch: 'https://rickandmortyapi.com/api/character',
   currentSearch: '',
   characters: [],
-
-  name: 'Concavo',
-  description: 'Concavo React Tailwind CSS admin template',
-  url: 'https://concavo.mobifica.com',
-  layout: 'layout-1',
-  direction: 'ltr',
-  collapsed: false,
-  toggleRightSidebar: false,
-  colors: [
-    'gray',
-    'red',
-    'orange',
-    'yellow',
-    'green',
-    'teal',
-    'blue',
-    'indigo',
-    'purple',
-    'pink'
-  ],
-  palettes: {
-    background: 'white',
-    logo: 'white',
-    leftSidebar: 'white',
-    rightSidebar: 'white',
-    navbar: 'white',
-    topNavigation: 'white'
-  },
-  leftSidebar: {
-    showButtonText: true,
-    showSectionTitle: true,
-    showLogo: true,
-    showCard: true,
-    showAccountLinks: false,
-    showProjects: true,
-    showTags: true,
-    card: 1
-  },
 }
 
 const reducer = (state = initialState, action) => {
@@ -85,58 +47,6 @@ const reducer = (state = initialState, action) => {
         characters: action.characters
       }
 
-    case 'SET_DEMO':
-      return {
-        ...state,
-        layout: action.layout,
-        collapsed: action.collapsed,
-        direction: action.direction,
-        leftSidebar: {
-          ...action.leftSidebar
-        },
-        palettes: {
-          ...action.palettes
-        }
-      }
-    case 'SET_PALETTE':
-      return {
-        ...state,
-        palettes: {
-          ...state.palettes,
-          [`${action.key}`]: action.value
-        }
-      }
-    case 'SET_LEFT_SIDEBAR_CONFIG':
-      return {
-        ...state,
-        leftSidebar: {
-          ...state.leftSidebar,
-          [`${action.key}`]: action.value
-        }
-      }
-    case 'SET_LAYOUT':
-      if (action.layout === 'layout-3' || action.layout === 'layout-4') {
-        return {
-          ...state,
-          layout: action.layout,
-          collapsed: true
-        }
-      }
-      return {
-        ...state,
-        layout: action.layout,
-        collapsed: false
-      }
-      return {
-        ...state,
-        [`${key}`]: value
-      }
-    case 'SET_CONFIG':
-      let { key, value } = { ...action.config }
-      return {
-        ...state,
-        [`${key}`]: value
-      }
     default:
       return state
   }
